928ef4e595f429d72a7afbe1042f82ce3117a11d
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-23 20:34:16 -0600

    Carpeta Principal

---
+++ paginaweb_vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import PaginaPrincipal from './components/PaginaPrincipal.vue';
+</script>
+
+<template>
+  
+  <PaginaPrincipal msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
