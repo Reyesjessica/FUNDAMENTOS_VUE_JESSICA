e733ebdb72cf9103cc07752ebb41ba4fccccbf69
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2025-03-23 21:40:42 -0600

    Componente para seccion

--- paginaweb_componentes/src/components/PaginaPrincipal2.vue
+++ paginaweb_componentes/src/components/PaginaPrincipal2.vue
@@ -3,6 +3,7 @@ import encabezado from './encabezado.vue';
 import Pmenu from './Pmenu.vue';
 import carosel from './carosel.vue';
 import cardPostre from './cardPostre.vue';
+import seccion from './seccion.vue';
 </script>
 
 <template>
@@ -16,28 +17,7 @@ import cardPostre from './cardPostre.vue';
   <!-- Card de postre -->
   <cardPostre/>
       <!-- seccion de parrafos y textos -->
-      <section class="nosotros my-5">
-    
-    <div class="container">
-      <div class="row">
-        <h2 style="font-family: Berlin - Fuentes de Letras Redondas;" > Bienvenidos COFFE </h2>
-        <div class="col-md-6">
-          <p style="text-align: justify; color: rgb(7, 7, 92);font-size: 18px;">En COFFE, no solo
-            servimos café, sino que creamos momentos especiales.
-            Nos apasiona ofrecerte una experiencia única, con sabores auténticos y
-            una atmósfera acogedora. Desde nuestros granos seleccionados a mano hasta
-            las delicadas combinaciones de sabores, cada taza está hecha con amor y dedicación.</p>
-        </div>
-        <div class="col-md-6">
-          <img style=" height: 170px ;width: 450px;" class="img-fluid"
-            src="https://th.bing.com/th/id/R.6f5c50013936c54d16e3eb739dab06a4?rik=wFYawKzbTg5hgA&pid=ImgRaw&r=0"
-            alt="bebidas">
-
-
-        </div>
-      </div>
-    </div>
-  </section>
+     <seccion/>
   <!-- Card de ofertas -->
   <section class="OFERTAS my-5">
     <h2 style="font-family:'Times New Roman', Times, serif; color:#320000;" class="text-center my-5"> OFERTAS</h2>
